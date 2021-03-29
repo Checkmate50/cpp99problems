9c18f491080a036f3e3d3501854733d0d0921a43
Checkmate50 <dgeisler50@gmail.com>
2021-03-29 18:32:45 -0400

    Fixes to problems 47 and 48

--- p4650/logic.cpp
+++ p4650/logic.cpp
@@ -28,115 +28,59 @@ bool myequ(bool l, bool r) {
     return l == r;
 }
 
-logic_infix_struct& operator&(const bool& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_op('&');
-    return logic_infix_object;
+and_infix_struct& operator&(const bool& lhs, const logic_infix_struct& rhs) {
+    and_infix_object.set_lhs(lhs);
+    return and_infix_object;
 }
-logic_infix_struct& operator&(const logic_infix_struct& lhs, const bool& rhs) {
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('&');
-    return logic_infix_object;
+bool operator&(const logic_infix_struct& lhs, const bool& rhs) {
+    and_infix_object.set_rhs(rhs);
+    return (bool)and_infix_object;
 }
-logic_infix_struct& operator&(const logic_infix_struct& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('&');
-    return logic_infix_object;
+or_infix_struct& operator|(const bool& lhs, const logic_infix_struct& rhs) {
+    or_infix_object.set_lhs(lhs);
+    return or_infix_object;
 }
-logic_infix_struct& operator|(const bool& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_op('|');
-    return logic_infix_object;
+bool operator|(const logic_infix_struct& lhs, const bool& rhs) {
+    or_infix_object.set_rhs(rhs);
+    return (bool)or_infix_object;
 }
-logic_infix_struct& operator|(const logic_infix_struct& lhs, const bool& rhs) {
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('|');
-    return logic_infix_object;
+nand_infix_struct& operator+(const bool& lhs, const logic_infix_struct& rhs) {
+    nand_infix_object.set_lhs(lhs);
+    return nand_infix_object;
 }
-logic_infix_struct& operator|(const logic_infix_struct& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('|');
-    return logic_infix_object;
+bool operator+(const logic_infix_struct& lhs, const bool& rhs) {
+    nand_infix_object.set_rhs(rhs);
+    return (bool)nand_infix_object;
 }
-logic_infix_struct& operator+(const bool& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_op('+');
-    return logic_infix_object;
+nor_infix_struct& operator-(const bool& lhs, const logic_infix_struct& rhs) {
+    nor_infix_object.set_lhs(lhs);
+    return nor_infix_object;
 }
-logic_infix_struct& operator+(const logic_infix_struct& lhs, const bool& rhs) {
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('+');
-    return logic_infix_object;
+bool operator-(const logic_infix_struct& lhs, const bool& rhs) {
+    nor_infix_object.set_rhs(rhs);
+    return (bool)nor_infix_object;
 }
-logic_infix_struct& operator+(const logic_infix_struct& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('+');
-    return logic_infix_object;
+xor_infix_struct& operator^(const bool& lhs, const logic_infix_struct& rhs) {
+    xor_infix_object.set_lhs(lhs);
+    return xor_infix_object;
 }
-logic_infix_struct& operator-(const bool& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_op('-');
-    return logic_infix_object;
+bool operator^(const logic_infix_struct& lhs, const bool& rhs) {
+    xor_infix_object.set_rhs(rhs);
+    return (bool)xor_infix_object;
 }
-logic_infix_struct& operator-(const logic_infix_struct& lhs, const bool& rhs) {
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('-');
-    return logic_infix_object;
+impl_infix_struct& operator*(const bool& lhs, const logic_infix_struct& rhs) {
+    impl_infix_object.set_lhs(lhs);
+    return impl_infix_object;
 }
-logic_infix_struct& operator-(const logic_infix_struct& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('-');
-    return logic_infix_object;
+bool operator*(const logic_infix_struct& lhs, const bool& rhs) {
+    impl_infix_object.set_rhs(rhs);
+    return (bool)impl_infix_object;
 }
-logic_infix_struct& operator^(const bool& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_op('^');
-    return logic_infix_object;
+equ_infix_struct& operator/(const bool& lhs, const logic_infix_struct& rhs) {
+    equ_infix_object.set_lhs(lhs);
+    return equ_infix_object;
 }
-logic_infix_struct& operator^(const logic_infix_struct& lhs, const bool& rhs) {
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('^');
-    return logic_infix_object;
-}
-logic_infix_struct& operator^(const logic_infix_struct& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('^');
-    return logic_infix_object;
-}
-logic_infix_struct& operator*(const bool& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_op('*');
-    return logic_infix_object;
-}
-logic_infix_struct& operator*(const logic_infix_struct& lhs, const bool& rhs) {
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('*');
-    return logic_infix_object;
-}
-logic_infix_struct& operator*(const logic_infix_struct& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('*');
-    return logic_infix_object;
-}
-logic_infix_struct& operator/(const bool& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_op('/');
-    return logic_infix_object;
-}
-logic_infix_struct& operator/(const logic_infix_struct& lhs, const bool& rhs) {
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('/');
-    return logic_infix_object;
-}
-logic_infix_struct& operator/(const logic_infix_struct& lhs, const logic_infix_struct& rhs) {
-    logic_infix_object.set_lhs(lhs);
-    logic_infix_object.set_rhs(rhs);
-    logic_infix_object.set_op('/');
-    return logic_infix_object;
+bool operator/(const logic_infix_struct& lhs, const bool& rhs) {
+    equ_infix_object.set_rhs(rhs);
+    return (bool)equ_infix_object;
 }
--- p4650/logic.hpp
+++ p4650/logic.hpp
@@ -2,7 +2,9 @@
 
 #include<iostream>
 #include<vector>
+#include<math.h>
 #include"print.hpp"
+#include<string>
 
 bool myand(bool l, bool r);
 
@@ -27,73 +29,128 @@ struct logic_infix_struct {
     void set_op(char op)    { this->op  = op;  }
     void set_lhs(bool lhs)  { this->lhs = lhs; }
     void set_rhs(bool rhs)  { this->rhs = rhs; }
-    operator bool() const {
-        switch (op) {
-        case '&': return myand(lhs, rhs);
-            break;
-        case '|': return myor(lhs, rhs);
-            break;
-        case '+': return mynand(lhs, rhs);
-            break;
-        case '-': return mynor(lhs, rhs);
-            break;
-        case '^': return myxor(lhs, rhs);
-            break;
-        case '*': return myimpl(lhs, rhs);
-            break;
-        case '/': return myequ(lhs, rhs);
-            break;
-        }
-        return false; // shouldn't trigger with normal use
+    virtual explicit operator bool() const = 0;
+};
+
+struct and_infix_struct : public logic_infix_struct {
+    explicit operator bool() const {
+        return myand(lhs, rhs);
     }
 };
 
-inline logic_infix_struct logic_infix_object; // this is some seriously dark magic
+struct or_infix_struct : public logic_infix_struct {
+    explicit operator bool() const {
+        return myor(lhs, rhs);
+    }
+};
 
-logic_infix_struct& operator&(const bool& lhs, const logic_infix_struct& rhs);
-logic_infix_struct& operator&(const logic_infix_struct& lhs, const bool& rhs);
-logic_infix_struct& operator&(const logic_infix_struct& lhs, const logic_infix_struct& rhs);
-logic_infix_struct& operator|(const bool& lhs, const logic_infix_struct& rhs);
-logic_infix_struct& operator|(const logic_infix_struct& lhs, const bool& rhs);
-logic_infix_struct& operator|(const logic_infix_struct& lhs, const logic_infix_struct& rhs);
-logic_infix_struct& operator+(const bool& lhs, const logic_infix_struct& rhs);
-logic_infix_struct& operator+(const logic_infix_struct& lhs, const bool& rhs);
-logic_infix_struct& operator+(const logic_infix_struct& lhs, const logic_infix_struct& rhs);
-logic_infix_struct& operator-(const bool& lhs, const logic_infix_struct& rhs);
-logic_infix_struct& operator-(const logic_infix_struct& lhs, const bool& rhs);
-logic_infix_struct& operator-(const logic_infix_struct& lhs, const logic_infix_struct& rhs);
-logic_infix_struct& operator^(const bool& lhs, const logic_infix_struct& rhs);
-logic_infix_struct& operator^(const logic_infix_struct& lhs, const bool& rhs);
-logic_infix_struct& operator^(const logic_infix_struct& lhs, const logic_infix_struct& rhs);
-logic_infix_struct& operator*(const bool& lhs, const logic_infix_struct& rhs);
-logic_infix_struct& operator*(const logic_infix_struct& lhs, const bool& rhs);
-logic_infix_struct& operator*(const logic_infix_struct& lhs, const logic_infix_struct& rhs);
-logic_infix_struct& operator/(const bool& lhs, const logic_infix_struct& rhs);
-logic_infix_struct& operator/(const logic_infix_struct& lhs, const bool& rhs);
-logic_infix_struct& operator/(const logic_infix_struct& lhs, const logic_infix_struct& rhs);
+struct nand_infix_struct : public logic_infix_struct {
+    explicit operator bool() const {
+        return mynand(lhs, rhs);
+    }
+};
 
-#define iand   &logic_infix_object&
-#define ior    |logic_infix_object|
-#define inand  +logic_infix_object+
-#define inor   -logic_infix_object-
-#define ixor   ^logic_infix_object^
-#define iimpl  *logic_infix_object*
-#define iequ   /logic_infix_object/
+struct nor_infix_struct : public logic_infix_struct {
+    explicit operator bool() const {
+        return mynor(lhs, rhs);
+    }
+};
+
+struct xor_infix_struct : public logic_infix_struct {
+    explicit operator bool() const {
+        return myxor(lhs, rhs);
+    }
+};
+
+struct impl_infix_struct : public logic_infix_struct {
+    explicit operator bool() const {
+        return myimpl(lhs, rhs);
+    }
+};
+
+struct equ_infix_struct : public logic_infix_struct {
+    explicit operator bool() const {
+        return myequ(lhs, rhs);
+    }
+};
+
+inline and_infix_struct     and_infix_object;
+inline or_infix_struct      or_infix_object;
+inline nand_infix_struct    nand_infix_object;
+inline nor_infix_struct     nor_infix_object;
+inline xor_infix_struct     xor_infix_object;
+inline impl_infix_struct    impl_infix_object;
+inline equ_infix_struct     equ_infix_object;
+
+// lots of operators defined here
+and_infix_struct& operator&(const bool& lhs, const logic_infix_struct& rhs);
+bool operator&(const logic_infix_struct& lhs, const bool& rhs);
+or_infix_struct& operator|(const bool& lhs, const logic_infix_struct& rhs);
+bool operator|(const logic_infix_struct& lhs, const bool& rhs);
+nand_infix_struct& operator+(const bool& lhs, const logic_infix_struct& rhs);
+bool operator+(const logic_infix_struct& lhs, const bool& rhs);
+nor_infix_struct& operator-(const bool& lhs, const logic_infix_struct& rhs);
+bool operator-(const logic_infix_struct& lhs, const bool& rhs);
+xor_infix_struct& operator^(const bool& lhs, const logic_infix_struct& rhs);
+bool operator^(const logic_infix_struct& lhs, const bool& rhs);
+impl_infix_struct& operator*(const bool& lhs, const logic_infix_struct& rhs);
+bool operator*(const logic_infix_struct& lhs, const bool& rhs);
+equ_infix_struct& operator/(const bool& lhs, const logic_infix_struct& rhs);
+bool operator/(const logic_infix_struct& lhs, const bool& rhs);
+
+// Here's where the actual macro magic happens
+#define iand   &and_infix_object&
+#define ior    |or_infix_object|
+#define inand  +nand_infix_object+
+#define inor   -nor_infix_object-
+#define ixor   ^xor_infix_object^
+#define iimpl  *impl_infix_object*
+#define iequ   /equ_infix_object/
 
 template<typename F>
 void print_truth_line(bool l, bool r, F tf) {
     print_bool(l);
-    std::cout << "\t";
+    std::cout << '\t';
     print_bool(r);
-    std::cout << "\t";
+    std::cout << '\t';
     print_bool(tf(l, r));
     std::cout << std::endl;
 }
 
 template<typename F>
+void print_truth_line(std::vector<bool> v, F tf) {
+    for (int i = 0; i < v.size(); i++) {
+        print_bool(v[i]);
+        std::cout << '\t';
+    }
+    print_bool(tf(v));
+    std::cout << std::endl;
+}
+
+template<typename F>
 void truth_table(F truth_function) {
+    static_assert(std::is_invocable_r_v<bool, F, bool, bool>, "Invalid argument to truth table, expected binary boolean function");
     print_truth_line(true,  true,   truth_function);
     print_truth_line(true,  false,  truth_function);
     print_truth_line(false, true,   truth_function);
     print_truth_line(false, false,  truth_function);
+}
+
+template<typename F>
+void truth_table_n(int size, F truth_function) {
+    static_assert(std::is_invocable_r_v<bool, F, std::vector<bool>>, "Invalid argument to truth table, expected n-ary boolean function");
+    if (size > 63) {
+        std::cout << "Invalid input size, cannot handle more than 63 booleans for display/sanity reasons" << std::endl;
+        return;
+    }
+    auto result = (unsigned long long int)pow(2.0, size);
+    for (unsigned long long int i = 0; i < result; i++) {
+        auto boolArr = i;
+        std::vector<bool> nextResult;
+        for (int j = 0; j < size; j++) {
+            nextResult.push_back(!(boolArr & 1));
+            boolArr >>= 1;            
+        }
+        print_truth_line(nextResult, truth_function);
+    }
 }
